5b913db4b74750cc08ef309e4b6807ae299e1f6e
Nattakarn <108313524+PNattakarn@users.noreply.github.com>
2025-06-27 10:48:47 +0700

    Update Daily login page
    Edit and Update Daily login page

--- Assets/Scripts/UI/Levellogin.cs
+++ Assets/Scripts/UI/Levellogin.cs
@@ -1,18 +1,18 @@
-using System.Collections;    
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class Levellogin : MonoBehaviour
 {
-    public int totalLevels = 0; // Total number of levels
+    /*
+   public int totalLevels = 0; // Total number of levels
 
-    public int unlockedLevels = 1; // Number of unlocked levels
+   public int unlockedLevels = 1; // Number of unlocked levels
 
-    private int LevelButton[] levelButtons; // Number of level buttons
+   private int LevelButton[] levelButtons; // Number of level buttons
 
     public Button[] lvlButtonlogin;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    //Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         int levelAt = PlayerPrefs.GetInt("LevelAt", 2); // Default to level 1 if not set
@@ -28,5 +28,47 @@ public class Levellogin : MonoBehaviour
     void Update()
     {
         
+    }*/
+
+    public Button[] lvlButtonlogin; // Array of level buttons
+
+    public Image[] Lock;
+
+    public Image[] Done;
+
+    private int highestLevel;
+
+    private void Start()
+    {
+        highestLevel = PlayerPrefs.GetInt("highestLavel", 1);
+
+        for (int i = 0; i < lvlButtonlogin.Length; i++)
+        {
+            int levelNum = i + 1;
+            if (levelNum > highestLevel)
+            {
+                lvlButtonlogin[i].interactable = false; // Disable button if level is not unlocked
+                lvlButtonlogin[i].GetComponent<Image>().sprite = Lock[i].sprite;
+                lvlButtonlogin[i].GetComponentInChildren<Text>().text = ""; // Change button text to "Locked"
+            }
+            else
+            {
+                lvlButtonlogin[i].interactable = true; // Enable button if level is unlocked    
+                lvlButtonlogin[i].GetComponentInChildren<Text>().text = "" + levelNum; // Set button text to level number
+                lvlButtonlogin[i].GetComponent<Image>().sprite = Done[i].sprite; // Change button sprite to "Done"
+            }
+        }
     }
+
+    public void LoadLevel(int levelNum)
+    {
+        SceneManager.LoadScene("Level_" + levelNum); // Load the scene corresponding to the level index
+    }
+
+    public void Reset()
+    {
+        PlayerPrefs.DeleteAll(); // Reset all PlayerPrefs data
+        PlayerPrefs.Save();
+    }
+
 }
